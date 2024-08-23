01110fc2b745df30d49aa3dcdcd6c95025ddd60f
Aaron Tate <aaronctate@gmail.com>
2024-08-24 05:25:35 +1000

    Convert to winget
    Converted as much of list as is still in use and available on winget as possible.
    
    Added new apps.

---
+++ Winget-Desktop.bat
@@ -0,0 +1,18 @@
+winget install --id Git.Git -e --source winget
+winget install --id DuongDieuPhap.ImageGlass --source winget
+winget install --id 7zip.7zip --source winget
+Winget install --id Mozilla.Firefox --source winget
+winget install --id Meta.Oculus --source winget
+winget install --id Logitech.GHUB --source winget
+winget install --id Eraser.Eraser --source winget
+winget install --id Valve.Steam --source winget
+winget install --id qBittorrent.qBittorrent --source winget
+winget install --id Discord.Discord --source winget
+winget install --id DominikReichl.KeePass --source winget
+winget install --id Nextcloud.NextcloudDesktop --source winget
+winget install --id AntibodySoftware.WizTree --source winget
+winget install --id WinSCP.WinSCP --source winget
+winget install --id CPUID.CPU-Z --source winget
+winget install --id Bitwarden.Bitwarden --source winget
+winget install --id ShareX.ShareX --source winget
+
